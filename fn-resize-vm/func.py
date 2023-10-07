7d14498007bcead3fc51ec2c919abe08083f57a6
tniita <takuya.niita@oracle.com>
2023-10-07 22:52:22 +0900

    for flexible

--- fn-resize-vm/func.py
+++ fn-resize-vm/func.py
@@ -42,10 +42,10 @@ def handler(ctx, data: io.BytesIO = None):
         message_id = headers["x-oci-ns-messageid"]
         ocpu = cfg["OCPU"]
         memory = cfg["OCPU"]
-        add_ocpu = ocpu if ocpu is not None and isinstance(
-            ocpu, float) else "1.0"
-        add_memory = memory if memory is not None and isinstance(
-            memory, float) else "1.0"
+        add_ocpu = ocpu if ocpu is not None and float(
+            add_ocpu) else "1.0"
+        add_memory = memory if memory is not None and float(
+            add_memory) else "1.0"
     except Exception as ex:
         print('ERROR: Missing Message ID in the header', ex, flush=True)
         raise
